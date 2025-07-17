348de6018e6def129e61b24ce0d20a1b21d28143
Manjunathan C <67182077+cmanjunathan45@users.noreply.github.com>
2025-07-17 13:06:02 +0530

    Update another static value SimpleJavaCalculator.java

--- src/simplejavacalculator/SimpleJavaCalculator.java
+++ src/simplejavacalculator/SimpleJavaCalculator.java
@@ -18,6 +18,7 @@ package simplejavacalculator;
 
 public class SimpleJavaCalculator {
    public static String test;
+   public static String test2;
    public static void main(String[] args) {
       try {
          UI uiCal = new UI();
